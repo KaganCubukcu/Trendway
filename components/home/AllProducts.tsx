b9311c22ea0e7b45d495f1c5a12223c6430e1380
Kağan Çubukçu <kagancancubukcu@gmail.com>
2024-07-26 14:25:54 +0300

    Implemented Loading Component for Product Page

--- components/home/AllProducts.tsx
+++ components/home/AllProducts.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Heading from "@/components/general/Heading";
 import ProductCard from "@/components/home/ProductCard";
 import { products } from "@/mocks/mock.json";
+import Loading from "@/app/loading";
+
 const AllProducts = () => {
   return (
-    <div>
-      <Heading text={"All Products"} />
-      <div className="flex md:gap-10  flex-wrap justify-center mt-2">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+    <React.Suspense fallback={<Loading />}>
+      <div>
+        <Heading text={"All Products"} />
+        <div className="flex md:gap-10  flex-wrap justify-center mt-2">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
-    </div>
+    </React.Suspense>
   );
 };
 
